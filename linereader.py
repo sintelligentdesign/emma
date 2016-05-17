8b7d44c00263916375f684dc70b390839d0c2a44
Ellie Cochran <sintelligentdesign@icloud.com>
2016-05-16 23:39:47 -0400

    Revert "Linked main and linereader"
    This reverts commit 564773663a332fe4988231fbacb21fa1d88e5c13.

--- linereader.py
+++ linereader.py
@@ -3,8 +3,8 @@ wordarray = []
 stem = {}
 leaf = {}
 
-def input():
-    userinput = raw_input('YOU: ')
+while 1 > 0:
+    userinput = raw_input('INPUT >> ')
     # todo: remove and take note of punctuation for punctuation model
     wordarray = userinput.split(' ')
     print wordarray
--- main.py
+++ main.py
@@ -1,56 +1,49 @@
 import random
 import ast
 
-import linereader
-
+# load up the brain from reader. we'll link these later
 brain = ""
+brainfile = open("testbrain.brn", "r")
+for line in brainfile:
+    brain += line
+brain = ast.literal_eval(brain)
 
-while 1>0:
-    # load brain
-    brainfile = open("linetestbrain.brn", "r")
-    for line in brainfile:
-        brain += line
-    brain = ast.literal_eval(brain)
-    
-    # prompt user for input 
-    linereader.input()
-    
-    # REPLY LOGIC STARTS HERE
-    speaklength = random.randrange(8, 25)
-    sentence = ""
+speaklength = random.randrange(8, 25)
+sentence = ""
 
-    #choose first two words
-    sentence += random.choice(brain.keys())
-    #sets lives
-    lives = 2
+#choose first two words
+sentence += random.choice(brain.keys())
+#sets lives
+lives = 2
 
-    # create the sentence
-    while speaklength > 0 and lives > 0:
-        hasNext = False  #keeps track of dead end status
+# create the sentence
+while speaklength > 0 and lives > 0:
+    hasNext = False  #keeps track of dead end status
 
-        lastTwoWords = " ".join(sentence.split()[-2:])                              #gets last two words in sentence
-        for key in brain:                                                           #finds last two words
-            if key == lastTwoWords:                                                 #finds following words in brain
-                hasNext = True  #not dead end
-                totOccurances = 0
-                nextWordsDict = brain[key]
-                for word in nextWordsDict:                                          #finds total Occurances for RNG limit
-                    totOccurances += nextWordsDict[word]
+    lastTwoWords = " ".join(sentence.split()[-2:])                              #gets last two words in sentence
+    for key in brain:                                                           #finds last two words
+        if key == lastTwoWords:                                                 #finds following words in brain
+            hasNext = True  #not dead end
+            totOccurances = 0
+            nextWordsDict = brain[key]
+            for word in nextWordsDict:                                          #finds total Occurances for RNG limit
+                totOccurances += nextWordsDict[word]
 
-                selector = random.randrange(totOccurances) +1                       #RNG
-                for word in nextWordsDict:                                          #loops through next words, decrementing selector
-                    selector -= nextWordsDict[word]
-                    if selector <= 0:                                               #choses following word when selector equals zero
-                        sentence += " " + word
+            selector = random.randrange(totOccurances) +1                       #RNG
+            for word in nextWordsDict:                                          #loops through next words, decrementing selector
+                selector -= nextWordsDict[word]
+                if selector <= 0:                                               #choses following word when selector equals zero
+                    sentence += " " + word
 
-        if hasNext == False:            #if dead end, adds new stem, decrements lives
-            sentence += " " + random.choice(brain.keys())
-            lives -= 1
-            
-        speaklength -= 1
+    if hasNext == False:            #if dead end, adds new stem, decrements lives
+        sentence += " " + random.choice(brain.keys())
+        lives -= 1
+        
+    speaklength -= 1
 
-    if sentence.rfind('.') < 1:
-        sentence += "."
+if sentence.rfind('.') < 1:
+    sentence += "."
 
-    sentence = str.join("", sentence.splitlines())
-    print "HER: " + sentence
+sentence = str.join("", sentence.splitlines())
+
+print "OUTPUT: " + sentence
