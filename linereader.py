48a9a6734a446fa05e32caca3f2e79c62116cc8f
Ellie Cochran <sintelligentdesign@icloud.com>
2016-05-16 21:50:05 -0400

    Added tweaked version of reader.py designed to take line by line user input

---
+++ linereader.py
@@ -0,0 +1,40 @@
+wordarray = []
+stem = {}
+leaf = {}
+
+while 1 > 0:
+    userinput = raw_input('INPUT >> ')
+    # todo: remove and take note of punctuation for punctuation model
+    wordarray = wordarray + userinput.split(' ')
+
+    for count in range(0, len(wordarray)):
+        dupedetected = 0    # bit for carrying duplication detected message
+        if count < len(wordarray) - 2:  # so we don't go out of bounds
+            StemAsString = wordarray[count] + ' ' + wordarray[count + 1]        # take two words (the "stem")
+            LeafAsString = wordarray[count + 2]                                 # also get the word after the two words (the "leaf")
+            
+            # todo: figure out dups and ranking
+            for count in range(0, len(stem)):                                   # check through everything we've done so far to see if we have duplicate stem values
+                if StemAsString == stem.keys()[count]:                          # check StemAsString against each established previous first two word pair
+                    print "Duplicate stem detected: (" + StemAsString + "). Merging..."
+                    dupedetected = 1
+                            
+                    leaf = stem.values()[count]                                 # set value of leaf to CURRENT leaf
+                    
+                    for count in range(0, len(leaf)):                           # if we've already set this as a leaf, increment its score
+                        if leaf.keys()[count] == LeafAsString:
+                            leaf[LeafAsString] += 1
+                        else:                                                   # otherwise, append new leaf to leaf list
+                            leaf[LeafAsString] = 1                          
+                    
+            if dupedetected == 0:
+                leaf = {LeafAsString: 1}                                        # create dict with leaf and ranking if current stem isn't a dupe of another stem
+            
+            stem[StemAsString] = leaf
+            
+    print "Parse complete. Dumping to testbrain.brn"
+    brainfile = open("linetestbrain.brn", "w")
+    print >>brainfile, stem
+    brainfile.close()
+    # todo: convert currentwords to json?
+    # todo: let user choose files to read from and write to
