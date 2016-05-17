b0c7f7e809f649bbb3843c4e6625a2ca8d6068c0
ashes303 <ash96@pitt.edu>
2016-05-16 22:00:23 -0400

    generates basic sentence

--- main.py
+++ main.py
@@ -7,18 +7,35 @@ brainfile = open("testbrain.brn", "r")
 for line in brainfile:
     brain += line
 brain = ast.literal_eval(brain)
-    
+
 speaklength = 20
-sentence = ""
+sentence = "the wheels"
 diesides = int
 
 # create the sentence
 while speaklength > 0:
-    word = random.choice(brain.keys())
-    sentence += word
-    
-    diesides = word.values()
-    for count in range(0, len(diesides)):
-        
-    
+#    word = random.choice(brain.keys())
+#    sentence += word
+
+#    diesides = word.values()
+#    for count in range(0, len(diesides)):
+
+    lastTwoWords = " ".join(sentence.split()[-2:])
+    for key in brain:
+        if key == lastTwoWords:
+            totOccurances = 0
+            nextWordsDict = brain[key]
+            for word in nextWordsDict:
+                totOccurances += nextWordsDict[word]
+
+            selector = random.randrange(totOccurances) +1
+            for word in nextWordsDict:
+                selector -= nextWordsDict[word]
+                if selector <= 0:
+                    sentence += " " + word
+
+#    sentence += " banana"
+
     speaklength -= 1
+
+print "OUTPUT: " + sentence
