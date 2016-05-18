dd8f11633cf18ae3d3d67c09314db35f3db923bf
Ellie Cochran <sintelligentdesign@icloud.com>
2016-05-18 05:38:57 -0400

    Started playing with nltk

---
+++ higherlevel.py
@@ -0,0 +1,12 @@
+import nltk, re, pprint
+
+def preprocess(document):
+    sentences = nltk.sent_tokenize(document)                        # NLTK default sentence segmenter
+    print sentences
+    sentences = [nltk.word_tokenize(sent) for sent in sentences]    # NLTK default word tokenizer
+    print sentences
+    sentences = [nltk.pos_tag(sent) for sent in sentences]          # NLTK default part-of-speech 
+    print sentences
+    return sentences
+    
+input = raw_input('INPUT >> ')
