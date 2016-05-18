daac8515910132dbb4070eaf14c8f494f230d2e5
ashes303 <ash96@pitt.edu>
2016-05-18 16:30:58 -0400

    Improved performance

--- linereader.py
+++ linereader.py
@@ -9,30 +9,26 @@ while 1 > 0:
     wordarray = userinput.split(' ')
 
     for count in range(0, len(wordarray)):
-        dupedetected = 0    # bool carrying "duplication detected" message
         if count < len(wordarray) - 2:  # so we don't go out of bounds
             StemAsString = wordarray[count] + ' ' + wordarray[count + 1]        # take two words (the "stem")
             LeafAsString = wordarray[count + 2]                                 # also get the word after the two words (the "leaf")
-            
+
             # todo: figure out dups and ranking
-            for count in range(0, len(stem)):                                   # check through everything we've done so far to see if we have duplicate stem values
-                if StemAsString == stem.keys()[count]:                          # check StemAsString against each established previous first two word pair
-                    print "Duplicate stem detected: (" + StemAsString + "). Merging..."
-                    dupedetected = 1
-                            
-                    leaf = stem.values()[count]                                 # set value of leaf to CURRENT leaf
-                    
-                    for count in range(0, len(leaf)):                           # if we've already set this as a leaf, increment its score
-                        if leaf.keys()[count] == LeafAsString:
-                            leaf[LeafAsString] += 1
-                        else:                                                   # otherwise, append new leaf to leaf list
-                            leaf[LeafAsString] = 1                          
-                    
-            if dupedetected == 0:
+            if StemAsString in stem:                                       # check for duplicate stems
+                print "Duplicate stem detected: (" + StemAsString + "). Merging..."
+
+                leaf = stem[StemAsString]                                 # set value of leaf to CURRENT leaf
+
+                if LeafAsString in leaf:                                # if duplicate leaf, increment score
+                    leaf[LeafAsString] += 1
+                else:                                                   # otherwise, append new leaf to leaf list
+                    leaf[LeafAsString] = 1
+
+            else:
                 leaf = {LeafAsString: 1}                                        # create dict with leaf and ranking if current stem isn't a dupe of another stem
-            
+
             stem[StemAsString] = leaf
-            
+
     print "Parse complete. Dumping to linetestbrain.brn"
     brainfile = open("linetestbrain.brn", "w")
     print >>brainfile, stem
--- main.py
+++ main.py
@@ -21,24 +21,22 @@ while speaklength > 0 and lives > 0:
     hasNext = False  #keeps track of dead end status
 
     lastTwoWords = " ".join(sentence.split()[-2:])                              #gets last two words in sentence
-    for key in brain:                                                           #finds last two words
-        if key == lastTwoWords:                                                 #finds following words in brain
-            hasNext = True  #not dead end
-            totOccurances = 0
-            nextWordsDict = brain[key]
-            for word in nextWordsDict:                                          #finds total Occurances for RNG limit
-                totOccurances += nextWordsDict[word]
+    if lastTwoWords in brain:
+        totOccurances = 0
+        nextWordsDict = brain[lastTwoWords]
+        for value in nextWordsDict.values():                                          #finds total Occurances for RNG limit
+            totOccurances += value
 
-            selector = random.randrange(totOccurances) +1                       #RNG
-            for word in nextWordsDict:                                          #loops through next words, decrementing selector
-                selector -= nextWordsDict[word]
-                if selector <= 0:                                               #choses following word when selector equals zero
-                    sentence += " " + word
+        selector = random.randrange(totOccurances) +1                       #RNG
+        for key in nextWordsDict:                                          #loops through next words, decrementing selector
+            selector -= nextWordsDict[key]
+            if selector <= 0:                                               #choses following word when selector equals zero
+                sentence += " " + key
 
-    if hasNext == False:            #if dead end, adds new stem, decrements lives
+    else:            #if dead end, adds new stem, decrements lives
         sentence += " " + random.choice(brain.keys())
         lives -= 1
-        
+
     speaklength -= 1
 
 if sentence.rfind('.') < 1:
