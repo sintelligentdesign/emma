35a40e80d7d97fefe658c59b6c9c863fae593551
ashes303 <ash96@pitt.edu>
2016-05-19 16:27:54 -0400

    pos-tagger now outputs sentence of tags

--- pos-tagger.py
+++ pos-tagger.py
@@ -1,21 +1,19 @@
 #delete me later
 import nltk, re, pprint
 
-sentencepos = []
+def preprocess(document):
+    sentences = nltk.sent_tokenize(document)                        # NLTK default sentence segmenter
+    print sentences
+    sentences = nltk.word_tokenize(str(sentences).strip('[]\''))          # NLTK default word tokenizer
+    print sentences
+    sentences = nltk.pos_tag(sentences)          # NLTK default part-of-speech
+    print sentences
+    tagsentence = ""
+    for count in range(0, len(sentences)):         # makes sentence of tags from list of tuples
+        tup = sentences[count]
+        tagsentence += tup[1] + " "
+    print tagsentence
+    return tagsentence
 
-def preprocess(sentence):
-    # get the parts of speech from the input
-    sentence = nltk.sent_tokenize(sentence)                         # NLTK default sentence segmenter (todo: this should go in the main logic later)
-    #sentence = [nltk.word_tokenize(sent) for sent in sentences]    # NLTK default word tokenizer (todo: this should go in the main logic later)
-                                                                    # note: do we want to use the multi word expression tokenizer instead (to help with glue words)?
-    sentence = nltk.word_tokenize(str(sentence).strip('[]'))      
-    sentence = nltk.pos_tag(sentence)                               # NLTK default part-of-speech tagger
-    print sentence
-    
-    # extract parts of speech as array from tuple
-    for count in range (0, len(sentence)):
-        sentencepos.append(sentence[count])
-        print sentencepos
-    
 input = raw_input('INPUT >> ')
 preprocess(input)
