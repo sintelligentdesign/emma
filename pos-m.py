6316464e041fd067180186eeb1b7afa87b6331e2
ashes303 <ash96@pitt.edu>
2016-05-19 19:29:58 -0400

    Added looping feature

--- pos-m.py
+++ pos-m.py
@@ -1,9 +1,9 @@
 # Parts of Speech Markov model generator
 # note: model contents are wiped on each run
 # todo: fix that: import contents of partsofspeech.mdl and use it while comparing in grok()
-import nltk, re, pprint
+import nltk, re, pprint, ast
 
-target = "partsofspeech.mdl"    # this isn't hard-coded just in case we want to reuse this code later
+target = raw_input("File to write to: ")
 
 def getPartsOfSpeech(sentence):
     # get the parts of speech from the input
@@ -11,7 +11,7 @@ def getPartsOfSpeech(sentence):
     sentence = nltk.word_tokenize(str(sentence).strip('[]\''))    # NLTK default word tokenizer           todo: this should be in the main program
     sentence = nltk.pos_tag(sentence)                             # NLTK default part-of-speech tagger
     print sentence
-    
+
     # make "sentences" of tags from list of tuples
     tagsentence = []
     # the above is clearly a list, but we're calling it a sentence because as far as the markov muncher cares, it is one
@@ -24,8 +24,7 @@ def getPartsOfSpeech(sentence):
 
 def grok(input):
     posarray = getPartsOfSpeech(input)
-    
-    stem = {}
+    stem = ast.literal_eval(open(target, "r").read())                          #prevents overwrite
     leaf = {}
 
     for count in range(0, len(posarray)):
@@ -35,7 +34,7 @@ def grok(input):
 
             if StemAsString in stem:                                            # check for duplicate stems
                 print "Duplicate stem detected: (" + StemAsString + "). Merging..."
-                
+
                 leaf = stem[StemAsString]                                       # set value of leaf to CURRENT leaf
 
                 if LeafAsString in leaf:                                        # if duplicate leaf, increment ranking
@@ -47,11 +46,12 @@ def grok(input):
                 leaf = {LeafAsString: 1}                                        # create dict with leaf and ranking if current stem isn't a dupe of another stem
 
             stem[StemAsString] = leaf
-            
+
     print "Parse complete. Dumping to" + target
     modelfile = open(target, "w")
     print >>modelfile, stem
     modelfile.close()
     # todo: convert to json?
 
-grok("The quick brown fox jumped over the lazy dog.")   # test sentence. todo: delete
+while 1>0:
+    grok(raw_input("INPUT: "))
