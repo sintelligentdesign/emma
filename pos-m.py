425e0c49a3b7f4d9fd7845c09c37c52ff0901ee2
Ellie Cochran <sintelligentdesign@icloud.com>
2016-05-19 15:25:40 -0400

    Minor fixes to make the functions play well together

--- pos-m.py
+++ pos-m.py
@@ -3,52 +3,48 @@
 # todo: fix that
 import nltk, re, pprint
 
-wordarray = []
 stem = {}
 leaf = {}
 
 target = "partsofspeech.mdl"
 
-def getPartsOfSpeech(document):
+def getPartsOfSpeech(sentence):
     # get the parts of speech from the input
-    sentences = nltk.sent_tokenize(document)                        # NLTK default sentence segmenter (todo: this should go in the main logic later.)
+    sentence = nltk.sent_tokenize(document)                        # NLTK default sentence segmenter (todo: this should go in the main logic later.)
     print sentences
-    sentences = [nltk.word_tokenize(sent) for sent in sentences]    # NLTK default word tokenizer
+    sentence = [nltk.word_tokenize(sent) for sent in sentences]    # NLTK default word tokenizer
     print sentences
-    sentences = [nltk.pos_tag(sent) for sent in sentences]          # NLTK default part-of-speech 
+    sentence = [nltk.pos_tag(sent) for sent in sentences]          # NLTK default part-of-speech tagger
     print sentences
     
     # extract parts of speech as array from tuple
     # (TODO)
 
-def grok(input)
-PartsOfSpeechAsArray = getPartsOfSpeech(input)
-userinput = raw_input('INPUT >> ')
-wordarray = userinput.split(' ')
+def grok(input):
+    posarray = getPartsOfSpeech(input)
 
-for count in range(0, len(wordarray)):
-    if count < len(wordarray) - 2:  # so we don't go out of bounds
-        StemAsString = wordarray[count] + ' ' + wordarray[count + 1]        # take two parts of speech (the "stem")
-        LeafAsString = wordarray[count + 2]                                 # also get one part of speech after the former two (the "leaf")
+    for count in range(0, len(posarray)):
+        if count < len(posarray) - 2:  # so we don't go out of bounds
+            StemAsString = posarray[count] + ' ' + posarray[count + 1]          # take two parts of speech (the "stem")
+            LeafAsString = posarray[count + 2]                                  # also get one part of speech after the former two (the "leaf")
 
-        # todo: figure out dupes and ranking
-        if StemAsString in stem:                                            # check for duplicate stems
-            print "Duplicate stem detected: (" + StemAsString + "). Merging..."
-            leaf = stem[StemAsString]                                       # set value of leaf to CURRENT leaf
+            if StemAsString in stem:                                            # check for duplicate stems
+                print "Duplicate stem detected: (" + StemAsString + "). Merging..."
+                leaf = stem[StemAsString]                                       # set value of leaf to CURRENT leaf
 
-            if LeafAsString in leaf:                                        # if duplicate leaf, increment score
-                leaf[LeafAsString] += 1
-            else:                                                           # otherwise, append new leaf to leaf list
-                leaf[LeafAsString] = 1
+                if LeafAsString in leaf:                                        # if duplicate leaf, increment ranking
+                    leaf[LeafAsString] += 1
+                else:                                                           # otherwise, append new leaf to leaf list
+                    leaf[LeafAsString] = 1
 
-        else:
-            leaf = {LeafAsString: 1}                                        # create dict with leaf and ranking if current stem isn't a dupe of another stem
+            else:
+                leaf = {LeafAsString: 1}                                        # create dict with leaf and ranking if current stem isn't a dupe of another stem
 
-        stem[StemAsString] = leaf
+            stem[StemAsString] = leaf
 
-print "Parse complete. Dumping to " target
-brainfile = open(target, "w")
-print >>brainfile, stem
-brainfile.close()
-# todo: convert currentwords to json?
-# todo: let user choose files to read from and write to
+    print "Parse complete. Dumping to " target
+    modelfile = open(target, "w")
+    print >>modelfile, stem
+    modelfile.close()
+    # todo: convert currentwords to json?
+    # todo: let user choose files to read from and write to
