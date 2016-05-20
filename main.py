749895fb663fc3c36995f6f9ea5c0acfe0bcc983
Ellie Cochran <sintelligentdesign@icloud.com>
2016-05-20 07:49:58 -0400

    Implemented bannedwords, minor bugfixes

--- main.py
+++ main.py
@@ -22,61 +22,72 @@ import utilities, nltk, conceptgen, posmodelgen
 inputAsParagraph = raw_input('You >> ')                     # since we're going to be responding to Tumblr asks, we'll assume that all input will be paragraph form.
                                                             # todo: add alternative ways to input
 
+bannedWordsTxt = open("bannedwords.txt", "r")               # load naughty words into a list so that we can screen for them
+bannedWords = []
+for line in bannedWordsTxt:
+    bannedWords.append(line)
+bannedWordsTxt.close()
+for count in range(0, len(bannedWords) - 1):
+    bannedWords[count] = bannedWords[count].rstrip('\n')
+
 inputAsSentences = nltk.sent_tokenize(inputAsParagraph)     # NLTK default sentence segmenter
+inputAsWords = []
+for sentence in range(0, len(inputAsSentences)):
+    # tokenize input sentence
+    inputAsWords.append(inputAsSentences[sentence])
+    inputAsWords = nltk.word_tokenize(inputAsSentences[sentence])           # NTLK default word tokenizer
+    posmodelgen.grok(inputAsWords)                                          # generate sentence model from input sentences
 
-for count in (0, len(inputAsSentences) - 1):
-    inputAsWords = inputAsSentences[count]
-    inputAsWords = nltk.word_tokenize(inputAsWords)         # NTLK default word tokenizer
-
-    posmodelgen.grok(inputAsWords)                          # generate sentence model from input sentences
-
-    # Generate concept using words in input sentences
+    # generate concept using words in input sentences
     nounCodes = ['NN', 'NNS', 'NNP', 'NNPS']
     verbCodes = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
     adjectiveCodes = ['JJ', 'JJR', 'JJS']
     inputAsPartsOfSpeech = posmodelgen.getPartsOfSpeech(inputAsWords)
 
-#find associations of nouns to other words
+# find associations of nouns to other words
 def conceptreader(inputAsWords, inputAsPartsOfSpeech):
     noun = ""
     associationType = 0
     association = ""
     proximity = 0
 
-    for count1 in range(0, len(inputAsWords)):                                  # finds a noun
+    for count1 in range(0, len(inputAsWords)):                                      # finds a noun
         if inputAsPartsOfSpeech[count1] in nounCodes:
-            noun = inputAsWords[count1]
+            if inputAsPartsOfSpeech[count1] not in bannedWords:
+                noun = inputAsWords[count1]
 
-            for count2 in range(count1 + 1, len(inputAsWords)):                 # looks for important word after noun   todo: turn this and the next code block into a function
-                importantWord = True
-                if inputAsPartsOfSpeech[count2] in nounCodes:
-                    associationType = 0
-                elif inputAsPartsOfSpeech[count2] in verbCodes:
-                    associationType = 1
-                elif inputAsPartsOfSpeech[count2] in adjectiveCodes:
-                    associationType = 2
+                for count2 in range(count1 + 1, len(inputAsWords)):                 # looks for important word after noun   todo: turn this and the next code block into a function
+                    importantWord = True
+                    if inputAsPartsOfSpeech[count2] in nounCodes:
+                        associationType = 0
+                    elif inputAsPartsOfSpeech[count2] in verbCodes:
+                        associationType = 1
+                    elif inputAsPartsOfSpeech[count2] in adjectiveCodes:
+                        associationType = 2
+                    else:
+                        importantword = False
+
+                    if importantWord:                                               # if an important word is found, add it to the concept graph
+                        association = inputAsWords[count2]
+                        proximity = count2 - count1
+                        conceptgen.addconcept(noun, associationType, association, proximity)
+
+                for count3 in range(0, count1):                                     # looks for important word before noun
+                    importantWord = True
+                    if inputAsPartsOfSpeech[count3] in nounCodes:
+                        associationType = 0
+                    elif inputAsPartsOfSpeech[count3] in verbCodes:
+                        associationType = 1
+                    elif inputAsPartsOfSpeech[count3] in adjectiveCodes:
+                        associationType = 2
+                    else:
+                        importantWord = False
+
+                    if importantWord:                                               # if an important word is found, add it to the concept graph
+                        association = inputAsWords[count3]
+                        proximity = count1 - count3
+                        conceptgen.addconcept(noun, associationType, association, proximity)
                 else:
-                    importantword = False
+                    pass                                                            # naughty words get put in the word passer to atone for their sins
 
-                if importantWord:                                               # if an important word is found, add it to the concept graph
-                    association = inputAsWords[count2]
-                    proximity = count2 - count1
-                    conceptgen.addconcept(noun, associationType, association, proximity)
-
-            for count3 in range(0, count1):                                     # looks for important word before noun
-                importantWord = True
-                if inputAsPartsOfSpeech[count3] in nounCodes:
-                    associationType = 0
-                elif inputAsPartsOfSpeech[count3] in verbCodes:
-                    associationType = 1
-                elif inputAsPartsOfSpeech[count3] in adjectiveCodes:
-                    associationType = 2
-                else:
-                    importantWord = False
-
-                if importantWord:                                               # if an important word is found, add it to the concept graph
-                    association = inputAsWords[count3]
-                    proximity = count1 - count3
-                    conceptgen.addconcept(noun, associationType, association, proximity)
-                    
-conceptreader(inputAsWords, inputAsPartsOfSpeech)
+#conceptreader(inputAsWords, inputAsPartsOfSpeech)
