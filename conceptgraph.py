bb20675b378ec435a672f030877b1073e401834a
Ellie Cochran <sintelligentdesign@icloud.com>
2016-05-20 00:07:57 -0400

    conceptgraph now commits new values into rows being updated

--- conceptgraph.py
+++ conceptgraph.py
@@ -1,21 +1,10 @@
-import sqlite3 as sql, sys, numpy as np
+import sqlite3 as sql, numpy as np
 
 connection = sql.connect('conceptgraph.db')     # connect to the concept graph SQLite database
     
 def addconcept(noun, associationType, association, proximity):
     with connection:
         cursor = connection.cursor()        # get the cursor object
-        
-        # ID
-        cursor.execute('SELECT MAX(id) FROM conceptgraph;')     # get max concept id from table
-        id = cursor.fetchone()
-        id = id[0] + 1                                          # add 1 to get new concept id
-                                                                # todo: do this in pure sql
-        print id                                                # todo: remove
-        
-        # NOUN todo: move this below dupe check
-        cursor.execute('SELECT * FROM conceptgraph WHERE noun = \'%s\'' % noun)     # check to see if noun exists in database
-        nounInDatabase = cursor.fetchall()
         
         
         # ASSOCIATION TYPE
@@ -24,27 +13,37 @@ def addconcept(noun, associationType, association, proximity):
         # ASSOCIATION
         
         
-        cursor.execute('SELECT * FROM conceptgraph WHERE noun = \'%s\' AND association = \'%s\'' % (noun, association))     # select the row that we want to work with
+        cursor.execute('SELECT * FROM conceptgraph WHERE noun = \'%s\' AND association = \'%s\';' % (noun, association))     # check to see if the row that we want to work with is already in the database
         row = cursor.fetchone()
         
-        if row != []:   # if the row is a duplicate, calculate its new values and add them
+        if row != []:                                                               # if the row is a duplicate, calculate its new values and add them
             # TOTAL FREQUENCY
-            totalfrequency = row[5]                                                 # get current total frequency
-            totalfrequency += totalfrequency                                        # add 1 for new total frequency
-            # todo: commit new total frequency
+            totalFrequency = row[5]                                                 # get current total frequency
+            totalFrequency += totalFrequency                                        # add 1 for new total frequency
             
             # AVERAGE PROXIMITY
-            averageproximity = row[6]                                               # get current average proximity
-            averageproximity = (averageproximity + proximity) + totalfrequency      # calculate new average proximity
-            # todo: commit new average proximity
+            avgProximity = row[6]                                                   # get current average proximity
+            avgProximity = (avgProximity + proximity) + totalfrequency              # calculate new average proximity
             
             # STRENGTH
-            strength = totalfrequency/(np.log(averageproximity) + 1)                # calculate new association strength
-            print strength
-            # todo: commit new strength
+            strength = calculatestrength(totalFrequency, avgProximity)              # calculate new association strength
             
-        else:           # if the row IS NOT a duplicate
-            print row   # todo: do other stuff
+            # COMMIT
+            cursor.execute('INSERT INTO conceptgraph (total_frequency, avg_proximity, strength) VALUES (\'%s\', \'%s\', \'%s\');' % (totalFrequency, avgProximity, strength))
+            
+        else:                                                                       # if the row IS NOT a duplicate
+            # ID
+            # note: max id should be auto-incremented, since it's a primary key. just in case, we'll keep this around
+            """
+            cursor.execute('SELECT MAX(id) FROM conceptgraph;')                     # get max concept id from table
+            id = cursor.fetchone()
+            id = id[0] + 1                                                          # add 1 to get new concept id
+            """"
+            print id                                                                # todo: remove
+            
+            # NOUN
+            cursor.execute('SELECT * FROM conceptgraph WHERE noun = \'%s\';' % noun) # check to see if noun exists in database
+            nounInDatabase = cursor.fetchall()
         
         # TOTAL FREQUENCY
         totalfrequency = 0
@@ -55,3 +54,7 @@ def addconcept(noun, associationType, association, proximity):
         # STRENGTH
     
 addconcept("hello", 0, "friend", 1)
+
+def calculatestrength(totalFreq, avgProx):
+    strength = totalFreq/(np.log(avgProx) + 1)
+    return strength
