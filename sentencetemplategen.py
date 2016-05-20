9cd0926b80b9eb502ab5f9435b0c391e6f977292
Ellie Cochran <sintelligentdesign@icloud.com>
2016-05-20 19:51:00 -0400

    sentencetemplategen is halfway finished

--- sentencetemplategen.py
+++ sentencetemplategen.py
@@ -5,35 +5,41 @@ import random, ast
 
 sentenceStructureModel = ast.literal_eval(open("sentencestructure.mdl", "r").read())    # load the generated sentence structure model
 
-sentenceTemplate = ""
-
-sentenceTemplate += random.choice(sentenceStructureModel.keys())                        # choose a stem at random from our sentence building block model
 
 # Sentence Generation
-while genLength > 0:
-    hasNext = False                                         # keeps track of dead end status
+def generate():
+    sentenceTemplate = []
+    sentenceTemplate += random.choice(sentenceStructureModel.keys())    # choose a stem at random from our sentence building block model
+    continueSentence = True
+    while continueSentence:
+        # todo: rewrite this using lists
+        lastTwoPOS = " ".join(sentenceTemplate.split()[-2:])            # retrieves the last two parts of speech we used
+        print lastTwoPOS
+        if lastTwoPOS in sentenceStructureModel:
+            totalOccurances = 0
+            nextPOSDict = sentenceStructureModel[lastTwoPOS]
+            for value in nextPOSDict.values():                          # finds total occurances for RNG limit
+                totalOccurances += value
 
-    lastTwoPOS = " ".join(sentence.split()[-2:])            # retrieves the last two parts of speech we used
-    if lastTwoPOS in sentenceStructureModel:
-        totalOccurances = 0
-        nextPOSDict = sentenceStructureModel[lastTwoPOS]
-        for value in nextPOSDict.values():                  # finds total occurances for RNG limit
-            totalOccurances += value
+            selector = random.randrange(totalOccurances) +1             # RNG
+            for key in nextPOSDict:                                     # loops through next parts of speech, decrementing selector
+                selector -= nextPOSDict[key]
+                if selector <= 0:                                       # choses following word when selector equals zero
+                    if key == "." or "?" or "!":
+                        sentenceTemplate += key                         # if the next key is punctuation, end the sentence
+                        continueSentence = False
+                    else:
+                        sentenceTemplate += " " + key                   # otherwise, append the part of speech
+                        print sentenceTemplate
 
-        selector = random.randrange(totalOccurances) +1     # RNG
-        for key in nextPOSDict:                             # loops through next parts of speech, decrementing selector
-            selector -= nextPOSDict[key]
-            if selector <= 0:                               # choses following word when selector equals zero
-                if key == "." or "?" or "!":
-                    sentenceTemplate += key                 # if the next key is punctuation, end the sentence
-                else:
-                    sentenceTemplate += " " + key           # otherwise, append the part of speech
+        else:                                                           # if we hit dead end, stick in a '%' so that we know that we hit a dead end and there should be more in the sentence model
+            sentenceTemplate += "%"
+            print "Sentence generation ended prematurely! (No leaves for stem %s)" % lastTwoPOS
+            continueSentence = False
+    
+    sentenceTemplate = str.join(" ", sentenceTemplate.splitlines())
+    # todo: keep this as a list?
 
-    else:                                                   # if we hit dead end, stick in a '%' so that we know that we hit a dead end and there should be more in the sentence model
-        sentenceTemplate += "%"
-        print "Sentence generation ended prematurely! (No leaves for stem %s)" % lastTwoPOS
-
-sentence = str.join("", sentenceTemplate.splitlines())
-# todo: keep this as a list?
-
-print "EMMA >> %s" % sentence
+    print "EMMA >> %s" % sentenceTemplate
+    
+generate()
