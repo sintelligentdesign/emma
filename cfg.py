98e731df3e528041282e16212c0340b82850fa28
Ellie Cochran <sintelligentdesign@icloud.com>
2016-05-23 11:45:03 -0400

    Added cfg

---
+++ cfg.py
@@ -0,0 +1,8 @@
+# Name:             Configuration File
+# Description:      Emma's "control panel"
+# Section:          
+# Writes/reads:     
+# Dependencies:     
+# Dependency of:    
+
+brain = 'emma'
