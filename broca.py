a88cfd7406a38b3bfc4171d0177926fb95ff1846
Ellie Cochran <sintelligentdesign@icloud.com>
2016-05-25 04:17:44 -0400

    Added "Broca's area" reply generation script

---
+++ broca.py
@@ -0,0 +1,6 @@
+# Name:             Broca's Area
+# Description:      Broca's Area ... is a region in the frontal lobe of the dominant hemisphere ... with functions linked to speech production.
+# Section:          REPLY
+# Writes/reads:     emma.brn/conceptgraph.db
+# Dependencies:     
+# Dependency of:    emma
