5773b2110f1d131eef68a84f537af3ea373da75f
ashes303 <ash96@pitt.edu>
2016-05-25 10:55:18 -0400

    insert verbs updated

--- broca.py
+++ broca.py
@@ -4,7 +4,7 @@
 # Writes/reads:     emma.brn/conceptgraph.db
 # Dependencies:     sqlite3, cfg
 # Dependency of:    emma
-import sqlite3 as sql, cfg
+import sqlite3 as sql, cfg, random
 
 connection = sql.connect('emma.brn/conceptgraph.db')        # connect to the concept graph SQLite database
 cursor = connection.cursor()                                # get the cursor object
@@ -36,12 +36,12 @@ def findrelatednouns(nounList, verbList):
         for count in range(0, len(nounList)):
             cursor.execute('SELECT * FROM conceptgraph WHERE noun = "%s" AND association_type = 0;' % nounList[count])
         SQLReturnNoun = cursor.fetchall()
-        
+
         for count in range(0, len(verbList)):
             cursor.execute('SELECT * FROM conceptgraph WHERE association = "%s" AND association_type = 1;' % verbList[count])
         SQLReturnVerb = cursor.fetchall()
         # todo: compare the two and merge
-        
+
 def findrelatedadjectives(GeneratedNounList):
     # note: this nounList is the list of nouns from our OUTPUT sentence
 
@@ -55,9 +55,24 @@ def insertverbs(sentenceTemplate, convonouns, relatedVerbs):
             verbList.append(pos)
             verbPosition.append(count)
 
-    # todo: make this work past just demo levelsf
-    for count in verbList[]
+
+    for count in range(0, len(verbList)):                                       # goes thru verb POS's
+        possiblewords = []
+        dietotal = 0.0
+        for verbtupe in relatedVerbs:                                           # matches related verbs by POS
+            if verbtupe[2] == verbList[count]:
+                possiblewords.append(verbtupe)
+        for verbtupe in possiblewords:                                          # rolls die weighted by strength of related matching verbs
+            dietotal += verbtupe[1]
+        dieroll = random.uniform(0, dietotal)
+        for verbtupe in possiblewords:
+            dieroll -= verbtupe[1]
+            if dieroll < 0:
+                sentenceTemplate[verbPosition[count]] = verbtupe[0]             # adds verb based on die to template
+                break
 
     print sentenceTemplate
-insertverbs(['VBP', 'JJ', 'NN', '.'], ['god', 'ponies'], [('take', 2.0, 'VBP'), ('taken', 1.3, 'VB')])
-insertverbs(['VBP', 'VB', 'NN', '.'], ['god', 'ponies'], [('take', 2.0, 'VBP'), ('taken', 1.3, 'VB')])
+insertverbs(['VBP', 'JJ', 'NN', '.'], ['god', 'ponies'], [('take', 2.0, 'VBP'), ('taken', 1.3, 'VB'), ('make', 1.3, 'VBP')])
+insertverbs(['VBP', 'VB', 'NN', '.'], ['god', 'ponies'], [('take', 2.0, 'VBP'), ('taken', 1.3, 'VB'), ('make', 1.3, 'VBP')])
+insertverbs(['VBP', 'VBP', 'NN', '.'], ['god', 'ponies'], [('take', 2.0, 'VBP'), ('taken', 1.3, 'VB'), ('make', 1.3, 'VBP')])
+insertverbs(['VBP', 'NN', 'VB', '.'], ['god', 'ponies'], [('take', 2.0, 'VBP'), ('taken', 1.3, 'VB'), ('make', 1.3, 'VBP')])
