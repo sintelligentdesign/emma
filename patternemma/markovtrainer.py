657acc1e30c82634c2f5fdf9674449d036f18041
Ellie Cochran <sintelligentdesign@icloud.com>
2016-05-29 09:13:14 -0400

    Added markovtrainer.py

---
+++ patternemma/markovtrainer.py
@@ -0,0 +1,6 @@
+# Name:             Sentence Template Markov model Trainer
+# Description:      Trains the sentenestructure database on sentence chunks parsed from our input data
+# Section:          LEARNING
+# Writes/reads:     emma.brn/conceptgraph.db
+# Dependencies:     
+# Dependency of:    
