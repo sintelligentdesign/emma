7cd543a264ef621cabeaacd1857f25b32c53a312
ashes303 <ash96@pitt.edu>
2016-05-28 21:57:26 -0400

    new config file

---
+++ patternemma/config.py
@@ -0,0 +1,25 @@
+# Name:             Configuration File
+# Description:      Emma's "control panel"
+# Section:
+# Writes/reads:
+# Dependencies:
+# Dependency of:
+
+# Auxillary functions classify POS
+def nounCodes(): return ['NN', 'NNS', 'NNP', 'NNPS']
+def verbCodes(): return ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+def adjectiveCodes(): return ['JJ', 'JJR', 'JJS']
+def adverbCodes(): return ['RB', 'RBR', 'RBS'] # should this include RP
+
+# contains the 'can have' relation between chunk and pos
+def posInChunk(pos, chunk):
+    wordsOfChunk = {'NP':['DT'] + adverbCodes() + adjectiveCodes() + nounCodes() + ['PRP'],
+        'PP':['TO', 'IN'],
+        'VP':adverbCodes() + ['MD'] + verbCodes(),
+        'ADVP':adverbCodes(),
+        'ADJP':['CC'] + adverbCodes() + adjectiveCodes(),
+        'SBAR':['IN'],
+        'PRT':['RP'],
+        'INTJ':['UH'],
+        'PRP':['PP','DT'] + adverbCodes() + adjectiveCodes() + nounCodes() + ['PRP']}
+    return True if chunk in wordsOfChunk and pos in wordsOfChunk[chunk] else False
