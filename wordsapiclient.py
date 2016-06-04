ff81985247183bbf23f119500c2cfed3b29f54ba
Ellie Cochran <sintelligentdesign@icloud.com>
2016-06-04 18:50:44 -0400

    made get_relation() actually work

--- wordsapiclient.py
+++ wordsapiclient.py
@@ -8,7 +8,9 @@ import unirest
 
 import apikeys
 
-def getRelation(word, relation):
+def get_relation(word, relationType):
+    # todo: what do we do with n-grams that are returned?
+    #       we should be able input a list for relationType and get a list of results in return to minimize API calls
     # Relations are:
     
     # hasParts      (house has door, floor, window)     return IS-PART-OF input
@@ -21,6 +23,15 @@ def getRelation(word, relation):
     # instanceOf    (einstein is instance of physicist) input IS-A return
     # hasTypes      (cat has tiger, housecat, lion)     return IS-A input
     
-    url = 'https://wordsapiv1.p.mashape.com/' + word + '/' + relation
+    url = 'https://wordsapiv1.p.mashape.com/words/' + word
     response = unirest.get(url, headers={"X-Mashape-Key": apikeys.wordsEnvironmentKey, "Accept": "application/json"})
-    return response
+    response = response.body
+    results = response["results"]
+    
+    relations = []
+    for result in results:
+        if relationType in result.keys():
+            relation = result[relationType]
+            relations.append(relation)
+    
+    return relations
