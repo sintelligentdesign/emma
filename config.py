3039e022f8ffb161e21f73bd0c7f9254ca2fb344
Ellie Cochran <sintelligentdesign@icloud.com>
2016-06-23 00:53:31 -0400

    Added new test ask

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ debug = {
 
     # If the above is false, supply a list of fake asks for Emma to read for testing and debugging
     'fakeAsks': [
-        ("12345", "hotpizzapie", u"I think you're fantastic. I don't know what I'd do without you."),
-        ("67890", "nanopup", u"Hi Emma! I hope you're doing well. I like dogs because they are gay.")]
+        #("12345", "hotpizzapie", u"I think you're fantastic. I don't know what I'd do without you."),
+        #("67890", "nanopup", u"Hi Emma! I hope you're doing well. I like dogs because they are gay."),
+        ("12345", "sparkplugiv", u"hi emma im gay")]
 }
 
 console = {
