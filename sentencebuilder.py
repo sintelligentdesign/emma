abf931a46b2bfc8e399fad5176ee588530e4eff2
Ellie Cochran <sintelligentdesign@icloud.com>
2016-07-03 20:02:33 -0400

    Changed how halos are created

--- sentencebuilder.py
+++ sentencebuilder.py
@@ -28,8 +28,18 @@ def generate_sentence(tokenizedMessage, asker=""):
             message.append(word[0])
             if word[1] in utilities.nounCodes and word[3] and word[0] not in importantWords:
                 importantWords.append(word[0])
+    
+    # Make common sense halo
+    print "Creating primary halo..."
+    primaryHalo = make_halo(importantWords)
+    print "Creating secondary halo..."
+    secondaryHalo = make_halo(primaryHalo)
+    for word in secondaryHalo:
+        if word in importantWords: primaryHalo.remove(word)
 
-    primaryHalo, secondaryHalo = make_common_sense_halo(importantWords)
+    print importantWords
+    print primaryHalo
+    print secondaryHalo
 
     '''
     reply = ['%']
@@ -44,7 +54,6 @@ def generate_sentence(tokenizedMessage, asker=""):
             newReply = expand_domains(importantWords, reply)
             if reply == newReply: domainsExpanded = True
             reply = newReply
-    '''
     
     reply[-1] += u"."
     reply[0] = reply[0][0].upper + reply[0][1:]
@@ -64,24 +73,16 @@ def generate_sentence(tokenizedMessage, asker=""):
             del reply[count]
 
     return ' '.join(reply)
+    '''
 
-def make_common_sense_halo(importantWords, depth=2):
-    print "Creating common sense halo..."
-    csHalo = importantWords
-    for i in range(0, depth):
-        foundWords = []
-        for word in csHalo:
-            with connection:
-                cursor.execute("SELECT target FROM associationmodel LEFT OUTER JOIN dictionary ON associationmodel.target = dictionary.word WHERE associationmodel.word = \'%s\' AND part_of_speech IN (\'NN\', \'NNS\', \'NNP\', \'NNPS\');" % word)
-                SQLReturn = cursor.fetchall()
-            for fetchedWord in SQLReturn:
-                if fetchedWord[0] not in csHalo: foundWords.extend(fetchedWord)
-        csHalo.extend(foundWords)
-
-    for word in csHalo:
-        if word in importantWords: csHalo.remove(word)
-
-    return csHalo
+def make_halo(words):
+    halo = []
+    for word in words:
+        with connection:
+            cursor.execute("SELECT target FROM associationmodel LEFT OUTER JOIN dictionary ON associationmodel.target = dictionary.word WHERE associationmodel.word = \'%s\' AND part_of_speech IN (\'NN\', \'NNS\', \'NNP\', \'NNPS\');" % word)
+            for fetchedWord in cursor.fetchall():
+                if fetchedWord not in words: halo.extend(fetchedWord)
+    return halo
 
 def get_associations(importantWords, csHalo):
     print "Finding associations..."
@@ -139,3 +140,5 @@ def makeInterrogative():
 
 def makePhrase():
     pass
+    
+generate_sentence([[[u'hi', u'UH', u'O', u'O'], [u'emma', u'NNP', u'B-NP', u'O'], [u'!', u'.', u'O', u'O']], [[u'sharkthemepark', 'NNP', u'B-NP', u'NP-SBJ-1'], [u'hope', u'VBP', u'B-VP', u'VP-1'], [u'emma', 'NNP', u'B-NP', u'NP-OBJ-1*NP-SBJ-2'], [u'be', u'VBP', u'B-VP', u'VP-2'], [u'do', u'VBG', u'I-VP', u'VP-2'], [u'well', u'RB', u'B-ADVP', u'O'], [u'.', u'.', u'O', u'O']], [[u'sharkthemepark', 'NNP', u'B-NP', u'NP-SBJ-1'], [u'like', u'VBP', u'B-VP', u'VP-1'], [u'dog', u'NNS', u'B-NP', u'NP-OBJ-1'], [u'because', u'IN', u'B-PP', u'O'], [u'dog', u'NNS', u'B-NP', u'NP-OBJ-1'], [u'be', u'VBP', u'B-VP', u'VP-2'], [u'gay', u'JJ', u'B-ADJP', u'O'], [u'.', u'.', u'O', u'O']]], u"sharkthemepark")
