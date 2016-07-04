80665de13c472f69205bc526aa09a1bcba9b7f27
Ellie Cochran <sintelligentdesign@icloud.com>
2016-07-03 21:46:03 -0400

    added package_associations()

--- sentencebuilder.py
+++ sentencebuilder.py
@@ -37,12 +37,17 @@ def generate_sentence(tokenizedMessage, asker=""):
     print "halo: " + str(halo)
 
     # Find associations
-    print "Finding associations..."
-    primaryAssociations = get_associations(importantWords)
-    secondaryAssociations = get_associations(halo)      # todo: create halo and get associations in one 
+    # Association package (information about bundle and the bundle itself) > Association bundle (word, all of its associations) > association (association type, target, weight)
+    print "Creating association bundles..."
+    primaryAssociations = bundle_associations(importantWords)
+    secondaryAssociations = bundle_associations(halo)      # todo: create halo and get associations in one 
     
-    print primaryAssociations
-    print secondaryAssociations
+    #print primaryAssociations
+    #print secondaryAssociations
+
+    # Create packages which include the association package and information about its contents so that the generator knows what domains can be used
+    print "Packaging associations and related information..."
+    primaryPackage = package_associations(primaryAssociations, asker)
 
     '''
     reply = ['%']
@@ -87,19 +92,20 @@ def make_halo(words):
                 if fetchedWord not in words: halo.extend(fetchedWord)
     return halo
 
-def get_associations(words):
-    # Associations are packaged as tuples. The first object is the word, and the second object is an array containing the association type, target, and weight.
-    associations = []
+def bundle_associations(words):
+    # Associations are bundled as tuples. The first object is the word, and the second object is an array containing the association type, target, and weight.
+    associationPackage = []
     for word in words:
+        print "Finding associations for \'%s\'..." % word
         with connection:
             cursor.execute("SELECT * FROM associationmodel WHERE word = \'%s\';" % word)
             SQLReturn = cursor.fetchall()
         if SQLReturn:
-            associationPackage = []
+            associationBundle = []
             for row in SQLReturn:
-                associationPackage.append([row[1], row[2], row[3]])
-            associations.append((word, associationPackage))
-    return associations
+                associationBundle.append([row[1], row[2], row[3]])
+            associationPackage.append([word, associationBundle])
+    return associationPackage
 
 def choose_association(associations):
     dieSeed = 0
@@ -111,11 +117,26 @@ def choose_association(associations):
             return row
             break
 
-def check_requirements(associations):
+def package_associations(associationBundle, asker):
     numObjects = 0
     hasAsker = False
     hasHas = False
     hasIsA = False
+    
+    numObjects = len(associationBundle)
+    if asker != "": hasAsker = True
+
+    associationPackage = []
+    for associationGroup in associationBundle:
+        for association in associationGroup[1]:
+            if "HAS" in association[0]: hasHas = True
+            if "IS-A" in association[0]: hasIsA = True
+        associationPackage.append([hasHas, hasIsA, associationGroup])
+    associationPackage = [{'numObjects': numObjects, 'hasAsker': hasAsker}, associationPackage]
+    #print associationPackage
+    return associationPackage
+
+package_associations([(u'blog', [[u'IS-A', u'blog', 0.0999999999997]]), (u'side', [[u'HAS-ABILITY-TO', u'know', 0.0999999999997], [u'HAS-ABILITY-TO', u'breathe', 0.0999999999997]]), (u'species', [[u'IS-OBJECT-OF', u'associate', 0.0999999999997], [u'IS-OBJECT-OF', u'be', 0.0999999999997], [u'IS-OBJECT-OF', u'live', 0.0999999999997], [u'HAS-ABILITY-TO', u'inhabit', 0.0999999999997]]), (u'matthew', [[u'HAS-ABILITY-TO', u'do', 0.0999999999997]]), (u'answer', [[u'HAS-ABILITY-TO', u'skip', 0.231969316683], [u'IS-OBJECT-OF', u'know', 0.0999999999997], [u'IS-OBJECT-OF', u'have', 0.231969316683], [u'IS-OBJECT-OF', u'come', 0.0999999999997], [u'IS-OBJECT-OF', u'inquire', 0.0999999999997], [u'IS-OBJECT-OF', u'find', 0.0999999999997], [u'IS-OBJECT-OF', u'validate', 0.0999999999997], [u'IS-OBJECT-OF', u'mean', 0.0999999999997], [u'IS-OBJECT-OF', u'die', 0.0999999999997], [u'IS-OBJECT-OF', u'name', 0.0999999999997]])], "sharkthemepark")
 
 # Define intents
 intents = ['COMPARATIVE', 'DECLARATIVE', 'IMPERATIVE', 'PHRASE']        # Greeting and Interrogative intents are special
