ff2c7f29ca5dddc42df4c05bc2ec393451414d4d
Ellie Cochran <sintelligentdesign@icloud.com>
2016-07-12 00:15:04 -0400

    Removed spell checker

--- utilities.py
+++ utilities.py
@@ -3,7 +3,6 @@
 # Description:      Miscellaneous functions and vars
 # Section:
 
-import enchant
 from colorama import init, Fore
 init(autoreset = True)
 
@@ -12,12 +11,3 @@ nounCodes = ['NN', 'NNS', 'NNP', 'NNPS']
 verbCodes = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
 adjectiveCodes = ['JJ', 'JJR', 'JJS']
 adverbCodes = ['RB', 'RBR', 'RBS', 'RP']
-
-d = enchant.Dict('en_US')
-def spellcheck(parsedSentence):
-    for count, row in enumerate(parsedSentence):
-        word = row[0]
-        if d.check(word) == False:
-            # todo: put what we want to do for words that flag the spell checker here
-            print Fore.RED + "Found possible mispelled word: \'%s\'" % word
-    return parsedSentence
