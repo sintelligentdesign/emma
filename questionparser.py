218a5a7ad74df5ea22f3f5616192be514c5abb83
Ellie Cochran <sintelligentdesign@icloud.com>
2016-07-20 10:11:28 -0400

    Added questionparser module

---
+++ questionparser.py
@@ -0,0 +1,26 @@
+# Name:             Question Parser
+# Description:      Reads sentences with an interrogative domain and tries to break the question into parts we can use
+# Section:          REPLY
+from colorama import init, Fore
+init(autoreset = True)
+
+import utilities
+from config import console
+
+def read_question(sentence):
+    questionType = ""
+    if sentence[0][0] == u"what" and sentence[2][0] == u"be":       # WHAT color BE the sky
+        interrogativeProperty = sentence[1][0]     # "color"
+        for word in sentence[::-1]:     # SKY the be color what
+            if word[1] in utilities.nounCodes:
+                interrogativeObject = word[0]        # "sky"
+                if console['verboseLogging']: print Fore.GREEN + "Interrogative: WHAT is " + interrogativeProperty + " of " + interrogativeObject
+                return (["what", interrogativeProperty, interrogativeObject])
+
+    elif sentence[0][0] == u"does" and sentence[2][0] == u"have":       # DO dog HAVE paw
+        interrogativeProperty = sentence[1][0]      # "dog"
+        for word in sentence[::-1]:     # PAW have dog do
+            if word[1] in utilities.nounCodes:
+                interrogativeObject = word[0]
+                if console['verboseLogging']: print Fore.GREEN + "Interrogative: DO " + interrogativeProperty + " HAVE " + interrogativeObject
+                return (["what", interrogativeProperty, interrogativeObject])
