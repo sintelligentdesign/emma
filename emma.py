42c514b72123ffeda3e71aaf94af536c12c08297
Ellie Cochran <sintelligentdesign@icloud.com>
2016-07-20 00:24:42 -0400

    fixed a silly but

--- emma.py
+++ emma.py
@@ -225,7 +225,7 @@ while True:
 
         print "Choosing activity..."
         activities = []
-        if debug['enableReblogs']: activities.append(reblogPost)
+        if debug['enableReblogs']: activities.append('reblogPost')
         if debug['enableDreams']: activities.extend(['dream'] * 2)
         if debug['enableReplies'] and askList != []: activities.extend(['replyToAsks'] * 3)
 
