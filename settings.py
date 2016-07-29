60b7baabd96ce28eb7eb0d286f90e9e83a22a56c
Ellie Cochran <sintelligentdesign@icloud.com>
2016-07-29 16:00:02 -0400

    Added back a function call

--- settings.py
+++ settings.py
@@ -10,6 +10,7 @@ settings = []
 def load_settings():
     global settings
     with open('settings.json', 'r') as settingsFile: settings = json.load(settingsFile)
+load_settings()
 
 def option(group, option): return settings[group][option]
 
