6d686318e262d7f805bf71065c03281d6584a448
Ellie Cochran <sintelligentdesign@icloud.com>
2016-07-29 03:25:06 -0400

    Added tumblr settings skeleton to settings GUI

--- config.py
+++ config.py
@@ -33,46 +33,49 @@ fakeAsks = [
 ]
 
 ## GUI stuff begins here
+def make_label(text, **kwds): return Label(text=text, **kwds)
+
 # General
 def update_general(setting):
     value = bool
     if setting in generalCheckboxMap.keys(): value = generalCheckboxMap[setting]
     general[setting] = value
-
-enableChatModeBox = CheckBox(
-    x = 20,
-    y = 45,
-    title = "Chat mode",
-    action = (update_general, 'enableChatMode')
-)
-enableSleepBox = CheckBox(
-    x = 20,
-    y = enableChatModeBox.bottom,
-    title = "Enable sleep",
-    action = (update_general, 'enableSleep')
-)
-verboseLoggingBox = CheckBox(
-    x = 20,
-    y = enableSleepBox.bottom,
-    title = "Verbose Logging",
-    action = (update_general, 'verboseLogging')
-)
-
+generalLabel = make_label("General", color=grey, x=20, y=15)
+enableChatModeBox = CheckBox(x=20, y=generalLabel.bottom, title="Chat mode", action=(update_general, 'enableChatMode'))
+enableSleepBox = CheckBox(x=20, y=enableChatModeBox.bottom, title="Enable sleep", action=(update_general, 'enableSleep'))
+verboseLoggingBox = CheckBox(x=20, y=enableSleepBox.bottom, title="Verbose Logging", action=(update_general, 'verboseLogging'))
 generalCheckboxMap = {'enableChatMode': enableChatModeBox.on, 'enableSleep': enableSleepBox.on, 'verboseLogging': verboseLoggingBox.on}
 
-win = Window(
-    width = 500,
-    height = 300,
-    title = "Emma Settings Panel"
-)
-#    height = enableChatModeBox.bottom + 20,
+# Tumblr
+def update_tumblr(setting):
+    value = bool
+    if setting in tumblrCheckboxMap.keys(): value = tumblrCheckboxMap[setting]
+    tumblr[setting] = value
+tumblrLabel = make_label("Tumblr", color=grey, x=20, y=verboseLoggingBox.bottom+15)
+publishOutputBox = CheckBox(x=20, y=tumblrLabel.bottom, title="Publish output", action=(update_tumblr, 'publishOutput'))
+enablePostPreviewBox = CheckBox(x=20, y=publishOutputBox.bottom, title="Show post preview", action=(update_tumblr, 'enablePostPreview'))
+enableAskRepliesBox = CheckBox(x=20, y=enablePostPreviewBox.bottom + 10, title="Enable Ask replies", action=(update_tumblr, 'enableAskReplies'))
+enableAskDeletionBox = CheckBox(x=20, y=enableAskRepliesBox.bottom, title="Enable Ask deletion", action=(update_tumblr, 'enableAskDeletion'))
+fetchRealAsksBox = CheckBox(x=20, y=enableAskDeletionBox.bottom, title="Fetch real Asks", action=(update_tumblr, 'fetchRealAsks'))
+enableReblogsBox = CheckBox(x=20, y=fetchRealAsksBox.bottom + 10, title="Enable Reblogs", action=(update_tumblr, 'enableReblogs'))
+enableDreamsBox = CheckBox(x=20, y=enableReblogsBox.bottom, title="Enable dreams", action=(update_tumblr, 'enableDreams'))
+tumblrCheckboxMap = {'publishOutput': publishOutputBox.on, 'enablePostPreview': enablePostPreviewBox.on, 'enableAskReplies': enableAskRepliesBox.on, 'enableAskDeletion': enableAskDeletionBox.on, 'fetchRealAsks': fetchRealAsksBox.on, 'enableReblogs': enableReblogsBox.on, 'enableDreams': enableDreamsBox.on}
 
-def make_label(text, **kwds): return Label(text = text, **kwds)
+win = Window(width=200, height=enableDreamsBox.bottom + 20, title="Emma Settings Panel")
 
-win.add(make_label("General", color=grey, x = 20, y = 20))
+win.add(generalLabel)
 win.add(enableChatModeBox)
 win.add(enableSleepBox)
 win.add(verboseLoggingBox)
-win.show()
 
+win.add(tumblrLabel)
+win.add(publishOutputBox)
+win.add(enablePostPreviewBox)
+win.add(enableAskRepliesBox)
+win.add(enableAskDeletionBox)
+win.add(fetchRealAsksBox)
+win.add(enableReblogsBox)
+win.add(enableDreamsBox)
+
+win.show()
 application().run()
