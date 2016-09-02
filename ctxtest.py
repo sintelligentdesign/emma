ba868d966c24f4b4187da6ad3f315be69cd555bf
Ellie Cochran <sintelligentdesign@icloud.com>
2016-09-02 17:44:27 -0400

    Added test cortex

---
+++ ctxtest.py
@@ -0,0 +1,7 @@
+import emma
+
+# Comment and uncomment messages as necessary
+message = "This is a test message."
+#message = "This is another test message."
+
+emma.input(message, 'sharkthemepark')
