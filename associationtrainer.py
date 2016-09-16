ca965d360bddaa3669ce3b8d58265d2614b5a95c
Ellie Cochran <sintelligentdesign@icloud.com>
2016-09-16 15:18:29 -0400

    Started to populate find_associations()

--- associationtrainer.py
+++ associationtrainer.py
@@ -16,13 +16,20 @@ cursor = connection.cursor()
 def find_associations(parsedSentence):
     print parsedSentence        # todo: Remove debug print statement
     for count, word in enumerate(parsedSentence):
-        ## Look for keywords
-        # Begin by looking for the word "be"
-        if word[0] == u"be":
-            pass
-        # Now look for the word "has"
-        if word[0] == u"has":
-            pass
 
-        ## Look for orders of word types
-        # todo
+        ## Look for keywords
+        if count != 0 and count - 1 != len(sentence):       # If this doesn't work __I will scream__
+
+            # Make lists out of the words before and after the word that we're on
+            wordsBackward = sentence[0:count]
+            wordsForeward = sentence[count + 1:-1]
+
+            # Begin by looking for the word "be"
+            if word[0] == u"be":
+                if "NP" in sentence[count] - 1: 
+            # Now look for the word "has"
+            if word[0] == u"has":
+                pass
+
+            ## Look for orders of word types
+            # todo
