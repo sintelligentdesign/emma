5142cee9aee2d6f7e379422b73e2365d62bd9bfe
Ellie Cochran <sintelligentdesign@icloud.com>
2016-09-16 14:06:30 -0400

    Began adding skeleton of find_associations()

--- associationtrainer.py
+++ associationtrainer.py
@@ -4,11 +4,25 @@
 import numpy as np
 import re
 
-import sqlite3 as sqlite3
+import sqlite3 as sql
 from colorama import init, Fore
 init(autoreset = True)
 
 import utilities
 
-connection = sql.connect('emma.db')
+connection = sql.connect("emma.db")
 cursor = connection.cursor()
+
+def find_associations(parsedSentence):
+    print parsedSentence        # todo: Remove debug print statement
+    for count, word in enumerate(parsedSentence):
+        ## Look for keywords
+        # Begin by looking for the word "be"
+        if word[0] == u"be":
+            pass
+        # Now look for the word "has"
+        if word[0] == u"has":
+            pass
+
+        ## Look for orders of word types
+        # todo
