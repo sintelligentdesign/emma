a12679535afb1de75fd25f922186fe78b2aa05b1
Ellie Cochran <sintelligentdesign@icloud.com>
2016-09-23 13:57:31 -0400

    Minor tweak

--- emma.py
+++ emma.py
@@ -16,9 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-# Name:             High-Level Emma Module
-# Description:      Controls high-level functions and decision making
-# Section:  
 import time
 import random
 import pickle
