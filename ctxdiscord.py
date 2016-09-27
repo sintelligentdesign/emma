a2255e20227eebb5c9c6771c142b97746e7868ae
Ellie Cochran <sintelligentdesign@icloud.com>
2016-09-27 15:23:52 -0400

    Added "add to a server" URL generation

--- ctxdiscord.py
+++ ctxdiscord.py
@@ -10,14 +10,18 @@ clientHeaders = {
     'Authorization': 'Bot ' + apikeys.discordClientToken
     }
 
+# Generate an 'add bot to guild' URL
+print "Add Emma to a server:"
+print "https://discordapp.com/oauth2/authorize?client_id=" + apikeys.discordClientID + "&scope=bot&permissions=0"
+
 # Test our connection
 r = requests.get('https://discordapp.com/api/oauth2/applications/@me', headers=clientHeaders)
 if r.status_code == "200":
     print "Connected to Discord!"
     # todo: pprint name, id
-elif r.status_code == "401":
+elif r.status_code in ["401", "403"]:
     print "Permissions error."
-    # todo: add flavor text
+    # todo: add flavor text, print json error response
 else:
     print "Discord connection failed."
     # todo: retry in 30s
