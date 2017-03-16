52b1a91c780ad9203c376dd2a8cd908bb9442bca
Ellie Cochran <sintelligentdesign@icloud.com>
2017-03-16 02:11:40 -0400

    Rewrote determine_posessive_references()
    I haven't tested it yet though. It /should/ work?

--- pronouns.py
+++ pronouns.py
@@ -1,9 +1,13 @@
+# Gets a Message object and iterates through sentences/words, replacing pronouns with the last used noun
 def determine_pronoun_references(message):
+    # Ideally I'd split pronouns into personal pronouns (she/her) and object pronouns (it/its) 
+    # so that the nouns that they reference could be tracked seperately
+    # but there are people who use it/its and similar pronouns so idk :/
     pronouns = [
-        "he", "him", "his", "himself",
-        "she", "her", "hers", "herself",
-        "they", "them", "their", "theirs", "themself", "themselves",
-        "it", "its", "itself"
+        'he', 'him', 'his', 'himself',
+        'she', 'her', 'hers', 'herself',
+        'they', 'them', 'their', 'theirs', 'themself', 'themselves',
+        'it', 'its', 'itself'
     ]
 
     # TODO: Rewrite all of this
@@ -15,24 +19,27 @@ def determine_pronoun_references(message):
                 lastUsedNoun = word
 
             elif word[0] in pronouns and lastUsedNoun != list:
-                print Fore.GREEN + u"Replacing pronoun \'%s\' with \'%s\'..." % (word[0], lastUsedNoun[0])
+                print Fore.GREEN + u'Replacing pronoun \'%s\' with \'%s\'...' % (word[0], lastUsedNoun[0])
                 sentence[count] = lastUsedNoun
 
     return message
     '''
 
-def determine_posessive_references(sentence, asker):
+# Gets a Message object and the name of the person sending the message and replaces posessive references (you/me/your/my/etc.) with the thing that they reference
+def determine_posessive_references(message, sender):
     # todo: add "'s" for posessives (your -> emma's) when we're able to do something with posessives
-    emmaReferences = [u"you", u"your", u"yours", u"yourself"]
-    askerReferences = [u"i", u"my", u"mine", u"myself"]
+    emmaReferences = [u'you', u'your', u'yours', u'yourself']
+    senderReferences = [u'i', u'my', u'mine', u'myself']
 
-    # TODO: Rewrite all of this
-    '''
-    for count, word in enumerate(sentence):
-        if word[0] in posessiveReferences.keys():
-            replacementWord = posessiveReferences[word[0]]
-            print Fore.GREEN + u"replacing posessive reference \'%s\' with \'%s\'..." % (word[0], replacementWord)
-            word[0] = replacementWord
-            word[1] = u"NNP"
-    return sentence
-    '''
+    for sentence in message.sentences:
+        for word in sentence.words:
+            if word.lemma in emmaReferences:
+                print logging.info("Replacing posessive reference \'%s\' with \'%s\'..." % (word.lemma, 'emma'))
+                word.lemma = u'emma'
+                word.partOfSpeech = 'NNP'
+            elif word.lemma in senderReferences:
+                print logging.info("Replacing posessive reference \'%s\' with \'%s\'..." % (word.lemma, sender))
+                word.lemma = sender
+                word.partOfSpeech = 'NNP'
+    
+    return message
