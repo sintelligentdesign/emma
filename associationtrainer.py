9d965914fbcf0f27cc1735a3087be080a01c6cbd
Ellie Cochran <sintelligentdesign@icloud.com>
2017-04-29 20:56:54 -0400

    Added has-object associations

--- associationtrainer.py
+++ associationtrainer.py
@@ -105,4 +105,7 @@ def find_associations(message):
 
                         # VB + obj >> VB HAS-OBJECT NN (This button releases the hounds. >> release HAS-OBJECT hound)
                         if "OBJ" in word.subjectObject and word.partOfSpeech in misc.nounCodes:
-                            pass
+                            for objectCandidate in sentence.words:
+                                # TODO: Add more criteria for choosing object associations
+                                if objectCandidate.partOfSpeech in misc.nounCodes:
+                                    train_association(objectCandidate.lemma, "HAS-OBJECT", word.lemma)
