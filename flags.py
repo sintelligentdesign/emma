c8591e58f5a60167687868f509ccc142e97fc3d2
Ellie Cochran <sintelligentdesign@icloud.com>
2017-05-21 13:07:17 -0400

    Added debug mode

--- flags.py
+++ flags.py
@@ -1,13 +1,20 @@
-# Testing Strings
-useTestingStrings = False
-testingStringType = 'simple'
+# Debug mode
+enableDebugMode = False
 
+# Testing Strings
+useTestingStrings = True
+testingStringType = 'simple'
+'''
 testingStringsSimple = [
     u"The sky is blue.",
     u"Dogs are very fluffy!",
     u"I want to be your friend.",
     u"I love pickles!",
     u"The quick brown fox jumped over the lazy dog."
+]
+'''
+testingStringsSimple = [
+    u"Bees are yellow and black."
 ]
 
 if testingStringType == 'simple':
