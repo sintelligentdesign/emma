1fee37698f1f19180f1ca979da436bc64ea0e8c5
Ellie Cochran <sintelligentdesign@icloud.com>
2017-05-21 12:26:26 -0400

    Added interrogative finder

--- wordpatternfinder.py
+++ wordpatternfinder.py
@@ -7,4 +7,8 @@ def find_patterns(message):
         # If the sentence ends in a question mark, it's proabably interrogative
         if sentence.words[-1].word == u"?":
             sentence.domain = 'interrogative'
+        # If the sentence starts with a wh-part of speech, it's also probably interrogative
+        if sentence.words[1].partOfSpeech in ['WDT', 'WP', 'WP$', 'WRB']:
+            sentence.domain = 'interrogative'
+
         
