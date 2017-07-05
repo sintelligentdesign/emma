9d89a4b3ff220dc224cae64c76c8dfd6b0d626dc
Ellie Cochran <sintelligentdesign@icloud.com>
2017-07-05 11:24:43 -0400

    Expanding > Electronic

--- emma.py
+++ emma.py
@@ -46,7 +46,7 @@ connection.text_factory = str
 cursor = connection.cursor()
 
 # Dumb chrome
-print u"\n .ooooo.  ooo. .oo.  .oo.   ooo. .oo.  .oo.    .oooo.\nd88' \u006088b \u0060888P\"Y88bP\"Y88b  \u0060888P\"Y88bP\"Y88b  \u0060P  )88b\n888ooo888  888   888   888   888   888   888   .oP\"888\n888    .,  888   888   888   888   888   888  d8(  888\n\u0060Y8bod8P' o888o o888o o888o o888o o888o o888o \u0060Y888\"\"8o\n\n        EXPANDING MODEL of MAPPED ASSOCIATIONS\n                     Version " + misc.versionNumber + "\n"
+print u"\n .ooooo.  ooo. .oo.  .oo.   ooo. .oo.  .oo.    .oooo.\nd88' \u006088b \u0060888P\"Y88bP\"Y88b  \u0060888P\"Y88bP\"Y88b  \u0060P  )88b\n888ooo888  888   888   888   888   888   888   .oP\"888\n888    .,  888   888   888   888   888   888  d8(  888\n\u0060Y8bod8P' o888o o888o o888o o888o o888o o888o \u0060Y888\"\"8o\n\n        ELECTRONIC MODEL of MAPPED ASSOCIATIONS\n                     Version " + misc.versionNumber + "\n"
 with connection:
     cursor.execute("SELECT * FROM associationmodel")
     associationModelItems = "{:,d}".format(len(cursor.fetchall()))
