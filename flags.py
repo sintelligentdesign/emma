21af14d889394caa6c23d858553a2252dcc245fb
Ellie Cochran <sintelligentdesign@icloud.com>
2018-05-16 16:24:14 -0400

    New test sentences

--- flags.py
+++ flags.py
@@ -11,7 +11,9 @@ testingStringsSimple = [
     # u"The sky is blue.",
     u"Dogs are very fluffy!",
     u"Dogs are cute.",
-    u"That dog is adorable!"
+    u"That dog is adorable!",
+    u"Dogs are animals.",
+    u"A building is a structure",
     # u"I want to be your friend.",
     # u"I love pickles!",
     # u"The quick brown fox jumped over the lazy dog."
